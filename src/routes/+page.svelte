86890088206d1e997d959372ec64c4c5079b68bc
Domenic Muentes <76541452+domuen@users.noreply.github.com>
2025-12-04 23:16:22 -0500

    feat: build out general page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Card from '../components/Card.svelte';
+</script>
+
+<span class="text-white text-3xl">Domenic Muentes</span>
+
+<br />
+<br />
+
+<div>
+	<span class="text-white">Information about me:</span>
+	<p class="text-white">- I am a programmer</p>
+
+    <br />
+
+	<span class="text-white">My personal projects:</span>
+
+    <br />
+    <br />
+
+	<div class="flex flex-row flex-wrap gap-3 max-w-fit bg-red-500">
+		<Card>
+			<span>Project 1</span>
+		</Card>
+
+		<Card>
+			<span>Project 2</span>
+		</Card>
+
+		<Card>
+			<span>Project 3</span>
+		</Card>
+
+		<Card>
+			<span>Project 4</span>
+		</Card>
+
+		<Card>
+			<span>Project 5</span>
+		</Card>
+	</div>
+</div>
