8596b5d5f2b95f980becde5111b2096efee786b8
Domenic Muentes <76541452+domuen@users.noreply.github.com>
2025-12-07 17:28:17 -0500

    feat: improve page layout & basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,57 @@
 <script lang="ts">
-	import Card from '../components/Card.svelte';
+	import ProjectCard from '../components/ProjectCard.svelte';
 </script>
 
-<span class="text-white text-3xl">Domenic Muentes</span>
+<!-- TODO: investigate this flex direction responsiveness bug -->
+<div class="flex lg:flex-col items-center p-4 gap-4">
+	<div class="flex lg:flex-row flex-col w-6xl gap-4 overflow-hidden">
+		<div class="flex flex-col lg:w-1/3 w-full p-6 gap-4 bg-gray-900 rounded-4xl">
+			<div class="flex gap-4">
+				<div class="w-20 h-20 bg-blue-500 rounded-full"></div>
+				<div>
+					<h2>Domenic Muentes</h2>
+					<h4>Software Engineer</h4>
+				</div>
+			</div>
 
-<br />
-<br />
+			<div class="lg:hidden flex gap-4">
+				<button class="text-white px-4 py-2 bg-gray-800 rounded-xl hover:bg-gray-700"
+					>View Resume</button
+				>
+				<button class="text-white px-4 py-2 bg-gray-800 rounded-xl hover:bg-gray-700"
+					>Contact</button
+				>
+			</div>
+		</div>
 
-<div>
-	<span class="text-white">Information about me:</span>
-	<p class="text-white">- I am a programmer</p>
+		<!-- <div class="h-px mx-6 bg-gray-500 rounded-full"></div> -->
 
-    <br />
+		<div class="flex flex-col lg:w-2/3 w-full p-6 gap-4 bg-gray-900 rounded-4xl">
+			<h2>My Personal Projects</h2>
 
-	<span class="text-white">My personal projects:</span>
+			<div class="flex lg:flex-row flex-col flex-wrap gap-3 max-w-fit">
+				<ProjectCard preview="" url="" />
+				<ProjectCard preview="" url="" />
+				<ProjectCard preview="" url="" />
+				<ProjectCard preview="" url="" />
+				<ProjectCard preview="" url="" />
+				<ProjectCard preview="" url="" />
+			</div>
+		</div>
+	</div>
 
-    <br />
-    <br />
+	<div class="lg:flex hidden lg:flex-row flex-col w-6xl gap-4 overflow-hidden">
+		<div class="flex flex-col lg:w-1/3 w-full p-6 gap-4 bg-gray-900 rounded-4xl">
+			<h2>Contact</h2>
 
-	<div class="flex flex-row flex-wrap gap-3 max-w-fit bg-red-500">
-		<Card>
-			<span>Project 1</span>
-		</Card>
+			<div class="h-px bg-gray-500 rounded-full"></div>
 
-		<Card>
-			<span>Project 2</span>
-		</Card>
+			<div>
+				<p>domenicmuentes@example.com</p>
+				<p>(555) 555-5683</p>
+			</div>
+		</div>
 
-		<Card>
-			<span>Project 3</span>
-		</Card>
-
-		<Card>
-			<span>Project 4</span>
-		</Card>
-
-		<Card>
-			<span>Project 5</span>
-		</Card>
+		<div class="lg:w-2/3 w-full"></div>
 	</div>
 </div>
