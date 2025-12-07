98092432192f0112fe6aab8c0ebcf1d9200810a3
Domenic Muentes <76541452+domuen@users.noreply.github.com>
2025-12-07 17:53:30 -0500

    feat: componentize main page

---
+++ src/components/ResumeCard.svelte
@@ -0,0 +1,7 @@
+<div class="lg:flex hidden flex-col w-full p-6 gap-4 bg-gray-900 rounded-4xl">
+	<h2>Resume</h2>
+
+	<div class="h-px bg-gray-500 rounded-full"></div>
+
+	<div class="w-24 h-43 bg-white"></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,25 @@
 <script lang="ts">
-	import ProjectCard from '../components/ProjectCard.svelte';
+	import PersonCard from '../components/PersonCard.svelte';
+	import ContactCard from '../components/ContactCard.svelte';
+	import ResumeCard from '../components/ResumeCard.svelte';
+
+	import PersonalProjectsCard from '../components/PersonalProjectsCard.svelte';
 </script>
 
 <div class="flex justify-center p-4 gap-4">
 	<div class="flex lg:flex-row flex-col w-6xl gap-4">
 		<!-- left column -->
 		<div class="flex flex-col lg:w-1/3 w-full gap-4">
-			<div class="flex flex-col w-full p-6 gap-4 bg-gray-900 rounded-4xl">
-				<div class="flex gap-4">
-					<div class="w-20 h-20 bg-blue-500 rounded-full"></div>
-					<div>
-						<h2>Domenic Muentes</h2>
-						<h4>Software Engineer</h4>
-					</div>
-				</div>
+			<PersonCard />
 
-				<div class="lg:hidden flex gap-4">
-					<button class="text-white px-4 py-2 bg-gray-800 rounded-xl hover:bg-gray-700"
-						>View Resume</button
-					>
-					<button class="text-white px-4 py-2 bg-gray-800 rounded-xl hover:bg-gray-700"
-						>Contact</button
-					>
-				</div>
-			</div>
+			<ContactCard />
 
-			<div class="lg:flex hidden flex-col w-full p-6 gap-4 bg-gray-900 rounded-4xl">
-				<h2>Contact</h2>
-
-				<div class="h-px bg-gray-500 rounded-full"></div>
-
-				<div>
-					<p>domenicmuentes@example.com</p>
-					<p>(555) 555-5683</p>
-				</div>
-			</div>
-
-			<div class="lg:flex hidden flex-col w-full p-6 gap-4 bg-gray-900 rounded-4xl">
-				<h2>Resume</h2>
-
-				<div class="h-px bg-gray-500 rounded-full"></div>
-
-				<div class="w-24 h-43 bg-white"></div>
-			</div>
+			<ResumeCard />
 		</div>
 
 		<!-- right column -->
 		<div class="flex flex-col lg:w-2/3 w-full gap-4">
-			<div class="flex flex-col w-full p-6 gap-4 bg-gray-900 rounded-4xl">
-				<h2>My Personal Projects</h2>
-
-				<div class="flex lg:flex-row flex-col flex-wrap gap-3 max-w-fit">
-					<ProjectCard preview="" url="" />
-					<ProjectCard preview="" url="" />
-					<ProjectCard preview="" url="" />
-					<ProjectCard preview="" url="" />
-					<ProjectCard preview="" url="" />
-					<ProjectCard preview="" url="" />
-				</div>
-			</div>
+			<PersonalProjectsCard />
 		</div>
 	</div>
 </div>
