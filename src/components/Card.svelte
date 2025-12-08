28d0723d8dff50b0f3d11294025f2eeac4c541a0
Domenic Muentes <76541452+domuen@users.noreply.github.com>
2025-12-08 11:16:31 -0500

    delete everything

--- src/components/Card.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	let { children } = $props();
-</script>
-
-<div class="bg-white rounded-lg p-2">{@render children()}</div>
--- src/components/ProjectCard.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	let { preview, url } = $props();
-</script>
-
-<div class="rounded-xl cursor:pointer hover:scale-1.25">
-	<img src={preview} alt="" />
-
-	<text>{url}</text>
-</div>
